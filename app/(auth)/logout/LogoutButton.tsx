876ceacd23291c2c4e3f7ea660378cca8012f233
Pauline Bihler <pauline.erika5@gmail.com>
2023-11-15 13:52:17 +0100

    Add route when user logs out

--- app/(auth)/logout/LogoutButton.tsx
+++ app/(auth)/logout/LogoutButton.tsx
@@ -1,12 +1,38 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
 import React from 'react';
 import { logout } from './actions';
 
 export default function LogoutButton() {
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await logout();
+    router.push('/');
+  };
+
   return (
-    <form>
-      <button className="logoutButton" formAction={logout}>
-        Logout{' '}
+    <form action="/logout" method="post">
+      <button className="logoutButton" onClick={handleLogout}>
+        Logout
       </button>
     </form>
   );
 }
+
+// // import { redirect } from 'next/navigation';
+// import React from 'react';
+// import { logout } from './actions';
+
+// // redirect('/login');
+
+// export default function LogoutButton() {
+//   return (
+//     <form>
+//       <button className="logoutButton" formAction={logout}>
+//         Logout{' '}
+//       </button>
+//     </form>
+//   );
+// }
