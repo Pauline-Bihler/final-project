a57554519d48f01170165996c41182aded61c6bd
Pauline Bihler <pauline.erika5@gmail.com>
2023-11-20 17:26:35 +0100

    Add css styling

--- app/forum/CreatePostsForm.tsx
+++ app/forum/CreatePostsForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 import { useRouter } from 'next/navigation';
 import { useState } from 'react';
+import styles from './forum.module.scss';
 
 export default function CreatePostsForm({ userId }: { userId: number }) {
   const [textContent, setTextContent] = useState('');
@@ -23,29 +24,38 @@ export default function CreatePostsForm({ userId }: { userId: number }) {
   }
 
   return (
-    <form
-      onSubmit={async (event) => {
-        event.preventDefault();
-        await handleCreatePost();
-      }}
-    >
-      <label>
-        Title:
-        <input
-          value={textTitle}
-          onChange={(event) => setTextTitle(event.currentTarget.value)}
-        />
-      </label>
-      <label>
-        Post any thoughts or questions:
-        <input
-          value={textContent}
-          onChange={(event) => setTextContent(event.currentTarget.value)}
-        />
-      </label>
-      <br />
-      <br />
-      <button>Post</button>
-    </form>
+    <div className={styles['overall-container']}>
+      <form
+        onSubmit={async (event) => {
+          event.preventDefault();
+          await handleCreatePost();
+        }}
+        className={styles['form']}
+      >
+        <div className={styles['form-row']}>
+          <label className={styles['label']}>
+            Title:
+            <input
+              value={textTitle}
+              onChange={(event) => setTextTitle(event.currentTarget.value)}
+              placeholder="Title"
+              className={styles['input']}
+            />
+          </label>
+        </div>
+        <div className={styles['form-row']}>
+          <label className={styles['label']}>
+            Post any thoughts or tips:
+            <input
+              value={textContent}
+              onChange={(event) => setTextContent(event.currentTarget.value)}
+              placeholder="thoughts or tips"
+              className={styles['input']}
+            />
+          </label>
+        </div>
+        <button className={styles['post-button']}>Post 🐾</button>
+      </form>
+    </div>
   );
 }
